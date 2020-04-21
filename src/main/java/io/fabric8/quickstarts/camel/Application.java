bcf4f818987db3fe50b67668992a25244548bf71
sit.sawan <sit.sawan@gmail.com>
2020-04-21 08:42:52 +0530

    3rd

--- src/main/java/io/fabric8/quickstarts/camel/Application.java
+++ src/main/java/io/fabric8/quickstarts/camel/Application.java
@@ -29,7 +29,7 @@ public class Application {
      * A main method to start this application.
      */
     public static void main(String[] args) {
-    	System.out.println("calling main method from test program 2..");
+    	System.out.println("calling main method from test program 3..");
         SpringApplication.run(Application.class, args);
     }
 
